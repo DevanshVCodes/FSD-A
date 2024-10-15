88a1debd886372d0a99aecb529b3781bb7ab523a
DevanshVCodes <devanshruchi@gmail.com>
2024-10-15 14:31:16 +0530

    Day5

---
+++ Day5/script.js
@@ -0,0 +1,15 @@
+// let x=confirm("Are you sure to logout")
+// alert(x);
+
+
+// let disp=()=>{
+//     console.log("welcome to arrow function in js")
+// }
+// disp();
+
+let sum=(x,y)=>{
+    return x+y;
+}
+
+let res=sum(12,32);
+console.log(res);
