628734f940925d31599781fa6b2b0b5a40aa37d1
DevanshVCodes <devanshruchi@gmail.com>
2024-10-18 09:23:26 +0530

    Day6 Part 1

---
+++ Day6/script.js
@@ -0,0 +1,8 @@
+let ar = [1,2,2,3,34,33,45];
+// let newar = ar.map((x)=>x*2);
+// console.log(newar);
+// let newar = ar.filter((x)=>x%2==0).map((x)=>x*3);
+// let newar = ar.reduce((x,y) => x+y,0);
+// let newar = ar.filter((x)=>x%2==0).map((x)=>x*4).reduce((x, y) => x+y, 0);
+console.log(newar);
+
