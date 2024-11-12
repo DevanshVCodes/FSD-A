f848fc51a248745cecad9c8e50a48b970befeb84
DevanshVCodes <devanshruchi@gmail.com>
2024-11-12 15:37:59 +0530

    Day9

--- Day8/vite-project/src/App.jsx
+++ Day8/vite-project/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
-const App = () => {
+
+const App = ({Name,Mail, Phone}) => {
   return (
-    <div>Devansh Vashishtha</div>
-  )
-}
+    <div id="disp">
+      <h1>Student Details</h1>
+      <h2>Name : {Name}</h2>
+      <h2>Email id : {Mail}</h2>
+      <h2>Mobile Number : {Phone}</h2>
+    </div>
+  );
+};
 
 export default App
--- Day8/vite-project/src/main.jsx
+++ Day8/vite-project/src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// import Marks from './Marks.jsx';
+import DisplayMarks from './DisplayMarks.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <App Name={'Devansh Vashishtha'} Mail={'devansh131202@gmail.com'} Phone={9548318252}/>
+    <Marks m1={87} m2={85} m3={84}/>
+    <Marks m1={90} m2={90} m3={89}/>
+    <DisplayMarks />
+  </StrictMode>
+);
