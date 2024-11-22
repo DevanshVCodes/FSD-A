e603f57cf124fad9022ef4269412055313329b25
DevanshVCodes <devanshruchi@gmail.com>
2024-11-22 10:27:53 +0530

    Day 11

---
+++ Day11/counter-app/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const[count,setCount] = useState(0);
+  return (
+  <div className="count-app">
+    <h1 className="count-disp">{count}</h1>
+    <div className="count-body">
+      <button className="count-btn"
+        onClick={()=>{
+          setCount(count+1);
+      }}
+      > Increment</button>
+      <button className="count-btn"
+        onClick={()=>{
+          setCount(0);
+        }}>
+          Reset
+        </button>
+        <button className="count-btn"
+        onClick={()=>{
+          setCount(count-1);
+        }}>
+          Decrement
+        </button>
+    </div>
+    </div>
+  );
+};
+export default App;
---
+++ Day11/counter-app/src/ImageEditor.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import img from "./Images/img.avif";
+
+const ImageEditor = () => {
+    const[width,setWidth] = useState(500);
+    const[height,setHeight] = useState(400);
+  return (
+    <div>
+        <img src={img} style={{width: `${width}px`, height:`${height}px`}}/>
+        <button
+            onClick={()=>{
+                setWidth(width+10);
+            }}>Increase Width</button>
+
+        <button
+            onClick={()=>{
+                setHeight(height+10);
+            }}>Increase Height</button>
+    </div>
+  );
+};
+export default ImageEditor;
